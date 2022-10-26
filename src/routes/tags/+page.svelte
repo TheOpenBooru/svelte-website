ec432f66322d4ee35d000636480adf8b0fddfc93
Ben Brady <benbradybusiness@gmail.com>
2022-10-26 14:10:43 +0100

    Added Tags Page

---
+++ src/routes/tags/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import HeadInfo from "lib/HeadInfo.svelte"
+	import TagsPage from "lib/Tags/Page.svelte"
+</script>
+
+<HeadInfo
+  title={`Tags`}
+  description={`Create and Edit Tags`}
+  path={`/tags`}
+/>
+<TagsPage/>
