2f53181b03e432a83174ef0b5318fed355bf9e5a
Ben Brady <benbradybusiness@gmail.com>
2022-10-26 14:09:18 +0100

    Updated TagAutocomplete

--- src/lib/AutoComplete.svelte
+++ src/lib/AutoComplete.svelte
@@ -1,34 +1,37 @@
 <script lang="ts">
   import type { Types } from "openbooru"
+	import { afterUpdate } from "svelte";
+  import { tagAutocomplete } from "js/booru"
   import Tag from "./Tag.svelte"
-
-  export const input: string = "";
-  export const callback: (tag: string) => void = console.log;
-
-  let tags: Types.Tag[] = []
+  
+  export let input: string;
+  export let callback: (tag: string) => void;
 </script>
 
-{#if tags && tags.length !== 0}
-  <div>
-    {#each tags as tag}
-      <Tag tagName={tag.name} data={tag} callback={() => callback(tag.name)} />
-    {/each}
-  </div>
-{/if}
+{#await tagAutocomplete(input, 8) then tags}
+  {#if tags.length > 0}
+    <div>
+      {#each tags as tag}
+        <Tag tagName={tag.name} data={tag} callback={() => callback(tag.name)} />
+      {/each}
+    </div>
+  {/if}
+{/await}
+
 
 <style>
   div {
     min-width: 8em;
     height: fit-content;
-    gap: 0.1rem;
     padding: 0.2rem;
-
+    
     background: var(--BACKGROUND-3);
     border: 0.2rem solid var(--BORDER-1);
     border-radius: 1rem;
-
+    
     display: flex;
     flex-direction: column;
     align-items: baseline;
+    gap: 0.5rem;
   }
 </style>
