573e984d49ea49affaa2f05ed4ab3a197ae5bdba
Ben Brady <benbradybusiness@gmail.com>
2022-10-30 16:14:33 +0000

    Proxy URL is Properly Used

--- src/js/proxy.ts
+++ src/js/proxy.ts
@@ -2,7 +2,7 @@ import { CORS_PROXY } from "js/config"
 
 export function generateUrl(url: string | undefined): string | undefined {
     if (CORS_PROXY && url && url.includes("rule34.xxx")) {
-        return "https://cdn.r34.pics/?url=" + encodeURIComponent(url);
+        return CORS_PROXY + encodeURIComponent(url);
     } else {
         return url;
     }
