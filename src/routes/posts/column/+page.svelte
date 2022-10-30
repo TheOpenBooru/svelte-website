2869c10f295df2e93b154a839006833bd9147bd9
Ben Brady <benbradybusiness@gmail.com>
2022-10-30 16:09:51 +0000

    Posts URL uses last used layout

--- src/routes/posts/column/+page.svelte
+++ src/routes/posts/column/+page.svelte
@@ -2,6 +2,11 @@
 	import PostsPage from "lib/Posts/Page.svelte";
 	import Column from "lib/Posts/Column/index.svelte"
 	import HeadInfo from "lib/HeadInfo.svelte";
+	import { browser } from "$app/environment";
+	
+	if (browser){
+		localStorage.setItem("last-posts", "column")
+	}
 </script>
 
 <HeadInfo path="/posts/column"/>
