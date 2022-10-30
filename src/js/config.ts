721550b070d8a894b836d0d1ed03e909fbfe867b
Ben Brady <benbradybusiness@gmail.com>
2022-10-30 16:11:44 +0000

    Added ProxyURL Config

--- src/js/config.ts
+++ src/js/config.ts
@@ -3,3 +3,4 @@ export const SITE_NAME = import.meta.env.VITE_SITE_NAME || "Open Booru";
 export const SITE_DESC = import.meta.env.VITE_SITE_DESC || "The Open Booru";
 export const SITE_URL = import.meta.env.VITE_SITE_URL || "localhost";
 export const READ_ONLY = import.meta.env.VITE_READ_ONLY === "true";
+export const CORS_PROXY = import.meta.env.VITE_VITE_CORS_PROXY || null;
