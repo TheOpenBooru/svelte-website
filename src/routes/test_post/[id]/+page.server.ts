d53f94b98694c1cf1984c0aa341733f0aed2da62
Ben Brady <benbradybusiness@gmail.com>
2022-11-07 16:35:14 +0000

    test_post debugging

--- src/routes/test_post/[id]/+page.server.ts
+++ src/routes/test_post/[id]/+page.server.ts
@@ -3,11 +3,16 @@ import { error } from '@sveltejs/kit';
 import { post_get } from 'js/booru';
 
 export const load: PageServerLoad = async ({ params }) => {
-  const post = await post_get(params.id)
+  let post;
+  try {
+    post = await post_get(params.id)
+  } catch (e) {
+    throw error(500, JSON.stringify(e));
+  }
   
   if (post) {
     return { post };
   } else {
-    throw error(404, 'Not found');
+    throw error(404, 'Post Not Found');
   }
 }
