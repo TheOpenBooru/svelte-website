94cea72fbad44bdd0bcf05ce9e3e85eec4bac526
Ben Brady <benbradybusiness@gmail.com>
2022-12-11 12:52:48 +0000

    Increased ssr post_search timeout

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -1,16 +1,14 @@
 import type { PageServerLoad } from './$types';
-import { API_KEY } from '$env/static/private';
-import { error } from '@sveltejs/kit';
-import { getBooruConfig } from 'js/booru';
-import { Posts, BSL } from 'openbooru';
+import { posts_search } from 'js/booru';
+import { BSL } from 'openbooru';
 
 export const load: PageServerLoad = async ({ url }) => {
 	const params = url.searchParams;
 	const query = BSL.decode(params.get("query") || "")
 	
-	const config = getBooruConfig();
-	config.token = API_KEY;
-	const posts = await Posts.search(query, 0, 100, config);
-
+	const posts = await Promise.any([
+		posts_search(query, 0, 100),
+		new Promise((resolve) => setTimeout(() => resolve([]), 1000))
+	])
 	return { posts };
 }
