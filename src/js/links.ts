7f22119dc1927acffdd5f70e3a504068d152f6d2
Ben Brady <benbradybusiness@gmail.com>
2022-12-11 12:49:11 +0000

    Added js/links

---
+++ src/js/links.ts
@@ -0,0 +1,7 @@
+import { ID } from "./booru";
+
+export const home = () => "/home";
+export const tags = () => "/tags";
+export const post = (id: number) => "/post/" + ID.encode(id);
+
+export default { home, tags, post }
