c41ca42445eb9fe2d408f6bc3d2cdf7618c7f523
Ben Brady <benbradybusiness@gmail.com>
2022-12-11 12:53:03 +0000

    Updated sitemap.xml

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,21 +1,24 @@
 import { API_URL, SITE_URL } from "js/config";
+import Links from "js/links";
 
 
+
+let cached_ids: number[] = [];
 let lastUpdated = new Date(0);
-let ids:number[] = [];
-
-async function updateIds(): Promise<number[]> {
-  const curDate = new Date()
+async function getIds(): Promise<number[]> {
+  const curDate = new Date();
   const timeSinceUpdated = (Number(curDate) - Number(lastUpdated)) / 1000;
+
   if (timeSinceUpdated > 3600) {
-    const r = await fetch(API_URL + "/posts/search");
-    const posts: Array<object> = await r.json();
-    ids = posts.map(post => post.id)
-    lastUpdated = new Date();
+    const r = await fetch(API_URL + "/posts/ids");
+    cached_ids = await r.json();
+    lastUpdated = curDate;
   }
+
+  return cached_ids
 }
 
-function generateUrlXML(path: string, changefreq: string, priority: string) {
+function generateSitemapUrl(path: string, changefreq: string, priority: string) {
   return "<url>" +
     `<loc>${SITE_URL + path}</loc>` +
     `<changefreq>${changefreq}</changefreq>` +
@@ -24,31 +27,31 @@ function generateUrlXML(path: string, changefreq: string, priority: string) {
 }
 
 export async function GET() {
-  const prefix = `<?xml version="1.0" encoding="UTF-8" ?>
-    <urlset
-      xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-      xmlns:xhtml="https://www.w3.org/1999/xhtml"
-      xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-      xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-      xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-    >
-  `;
+  const prefix =
+    `<?xml version="1.0" encoding="UTF-8"?>` +
+      `<urlset ` +
+      `xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" ` +
+      `xmlns:news="https://www.google.com/schemas/sitemap-news/0.9" ` +
+      `xmlns:xhtml="https://www.w3.org/1999/xhtml" ` +
+      `xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0" ` +
+      `xmlns:image="https://www.google.com/schemas/sitemap-image/1.1" ` +
+      `xmlns:video="https://www.google.com/schemas/sitemap-video/1.1" ` +
+    ">";
   const suffix = "</urlset>";
 
-  await updateIds();
   
-  let body = "";
-  body += prefix;
-  body += generateUrlXML("/", "monthly","1")
-  body += generateUrlXML("/posts", "monthly","1")
-  body += generateUrlXML("/info", "monthly","1")
-  body += generateUrlXML("/account", "monthly","1");
+  const urls: string[] = [];
+  urls.push(generateSitemapUrl("/", "monthly","1"));
+  urls.push(generateSitemapUrl("/posts", "monthly","1"));
+  urls.push(generateSitemapUrl("/info", "monthly","1"));
+  urls.push(generateSitemapUrl("/account", "monthly", "1"));
+  
+  const ids = await getIds();
   for (const id of ids) {
-    body += generateUrlXML(`/post/${id}`, "monthly", "0.9")
+    urls.push(generateSitemapUrl(Links.post(id), "weekly", "0.8"))
   }
-  body += suffix;
-  
+
+  const body = prefix + urls.join("") + suffix;
   return new Response(body, {
     headers: {
       'Cache-Control': 'max-age=3600',
