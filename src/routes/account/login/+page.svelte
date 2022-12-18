fde4024949e597875db8908139512d897e096b30
Ben Brady <benbradybusiness@gmail.com>
2022-12-18 14:26:07 +0000

    Added HeadInfo to login pages

--- src/routes/account/login/+page.svelte
+++ src/routes/account/login/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 	import Auth from "lib/Auth/index.svelte";
+	import HeadInfo from "lib/HeadInfo.svelte";
 </script>
 
+<HeadInfo
+	title="Login"
+	description="Login into your account"
+	path="/account/login"
+/>
 <Auth mode="login" />
--- src/routes/account/register/+page.svelte
+++ src/routes/account/register/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 	import Auth from "lib/Auth/index.svelte";
+	import HeadInfo from "lib/HeadInfo.svelte";
 </script>
 
+<HeadInfo
+	title="Register"
+	description="Register for a new account"
+	path="/account/register"
+/>
 <Auth mode="register" />
